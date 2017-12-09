4154760904a93e14c2063cc8bc222dd92fb4e8ef
Othosbro <othosbro@gmail.com>
2017-12-09 23:55:07 +0300

    Update utils.py

--- bitex/utils.py
+++ bitex/utils.py
@@ -14,7 +14,7 @@ import requests
 # Import Homebrew
 
 # Init Logging Facilities
-log = logging.getLogger(__name__)
+#log = logging.getLogger(__name__)
 
 
 def return_api_response(formatter=None):
@@ -31,28 +31,28 @@ def return_api_response(formatter=None):
             try:
                 r = func(*args, **kwargs)
             except Exception:
-                log.exception("return_api_response(): Error during call to %s(%s, %s)",
-                              func.__name__, args, kwargs)
+                #log.exception("return_api_response(): Error during call to %s(%s, %s)",
+                #              func.__name__, args, kwargs)
                 raise
 
             # Check Status
             try:
                 r.raise_for_status()
             except requests.HTTPError:
-                log.exception("return_api_response: HTTPError for url %s",
-                              r.request.url)
+                #log.exception("return_api_response: HTTPError for url %s",
+                 #             r.request.url)
 
             #  Verify json data
             try:
                 data = r.json()
             except json.JSONDecodeError:
-                log.error('return_api_response: Error while parsing json. '
-                          'Request url was: %s, result is: '
-                          '%s', r.request.url, r.text)
+                #log.error('return_api_response: Error while parsing json. '
+                 #         'Request url was: %s, result is: '
+                #          '%s', r.request.url, r.text)
                 data = None
             except Exception:
-                log.exception("return_api_response(): Unexpected error while parsing "
-                              "json from %s", r.request.url)
+                #log.exception("return_api_response(): Unexpected error while parsing "
+                 #             "json from %s", r.request.url)
                 raise
 
             # Format, if available
@@ -60,7 +60,7 @@ def return_api_response(formatter=None):
                 try:
                     r.formatted = formatter(data, *args, **kwargs)
                 except Exception:
-                    log.exception("Error while applying formatter!")
+                   # log.exception("Error while applying formatter!")
 
             return r
 
