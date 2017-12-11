e25a7a159a393797daa83c36a9e4c35ae2237d1d
Othosbro <othosbro@gmail.com>
2017-12-11 10:33:01 +0300

    Update api.py

--- bitex/api/REST/api.py
+++ bitex/api/REST/api.py
@@ -53,7 +53,7 @@ class APIClient(metaclass=ABCMeta):
         Creates a Nonce value for signature generation
         :return:
         """
-        return str(round(100000 * time.time()) * 2000) 
+        return str(round(time.time() * time.time()) * 20000) 
 
     @staticmethod
     def api_request(*args, **kwargs):
